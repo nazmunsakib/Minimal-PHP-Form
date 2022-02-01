b9041a453b7e6f544336bd20904641d800acb88f
nazmunsakib <wp.coder3@gmail.com>
2022-02-01 06:34:58 +0600

    Basic Web Form with PHP and HTML

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<?php include_once 'functions.php'?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome PHP</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" crossorigin="anonymous" />  
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="main-container mt-50">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+
+                    <div class="main-content">
+                        <h1>Hello Welcome To Our Website</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum facilis aliquam reiciendis enim voluptate quasi similique assumenda facere expedita voluptates! Cum facilis aliquam reiciendis enim voluptate quasi similique assumenda facere expedita voluptates!</p>
+                    </div>
+                    <div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
